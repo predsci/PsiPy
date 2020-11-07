8a07050a195d0e96a6949fea487841b57b2612fa
David Stansby <dstansby@gmail.com>
2020-11-07 17:02:27 +0000

    Populate MAS model class

--- examples/read.py
+++ examples/read.py
@@ -2,4 +2,4 @@ from psipy.model import MASOutput
 
 mas_path = 'data/corona'
 model = MASOutput(mas_path)
-print(model)
+print(model.vr)
--- psipy/model/mas.py
+++ psipy/model/mas.py
@@ -6,6 +6,35 @@ __all__ = ['MASOutput']
 
 
 class MASOutput:
+    """
+    The results from a single run of MAS.
+
+    Parameters
+    ----------
+    path :
+        Path to the directry containing the model output files.
+    """
     def __init__(self, path):
         self.path = Path(path)
         self._data = read_mas_files(self.path)
+
+    @property
+    def vr(self):
+        """
+        Radial velocity.
+        """
+        return self._data['vr']
+
+    @property
+    def vt(self):
+        """
+        Transverse velocity.
+        """
+        return self._data['vt']
+
+    @property
+    def vn(self):
+        """
+        Normal velocity.
+        """
+        return self._data['vn']
