1455d642ee4103d155d63c24b9beed16cebdc32b
David Stansby <dstansby@gmail.com>
2020-11-07 17:20:39 +0000

    Add plotting example

--- examples/read.py
+++ examples/read.py
@@ -1,5 +1,15 @@
 from psipy.model import MASOutput
+import matplotlib.pyplot as plt
 
 mas_path = 'data/corona'
 model = MASOutput(mas_path)
+vr = model.vr
 print(model.vr)
+
+vr_slice = vr.isel(r=-1)
+print(vr_slice)
+
+fig, ax = plt.subplots()
+vr_slice.plot(x='phi', y='theta', ax=ax)
+ax.set_aspect('equal')
+plt.show()
