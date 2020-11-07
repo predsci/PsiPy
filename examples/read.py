70a21304b0ad52510eadba612e04ccd78fdf5757
David Stansby <dstansby@gmail.com>
2020-11-07 17:26:51 +0000

    Add radial plotting capabilities

--- examples/read.py
+++ examples/read.py
@@ -3,13 +3,7 @@ import matplotlib.pyplot as plt
 
 mas_path = 'data/corona'
 model = MASOutput(mas_path)
-vr = model.vr
-print(model.vr)
-
-vr_slice = vr.isel(r=-1)
-print(vr_slice)
 
 fig, ax = plt.subplots()
-vr_slice.plot(x='phi', y='theta', ax=ax)
-ax.set_aspect('equal')
+model.vr.plot_radial_cut(-1, ax=ax)
 plt.show()
--- psipy/model/mas.py
+++ psipy/model/mas.py
@@ -6,6 +6,41 @@ from psipy.io.mas import _mas_vars
 __all__ = ['MASOutput']
 
 
+class Variable:
+    """
+    A single scalar variable.
+
+    This class contains methods for plotting data.
+
+    Parameters
+    ----------
+    data : xarray.DataArray
+        Variable data.
+    name : str
+        Variable name.
+    """
+    def __init__(self, data, name):
+        self.data = data
+        self.name = name
+
+    def plot_radial_cut(self, i, ax=None):
+        """
+        Parameters
+        ----------
+        i : int
+            Index at which to slice the data.
+        ax : matplolit.axes.Axes, optional
+            axes on which to plot. Defaults to current axes if not specified.
+        """
+        sliced = self.data.isel(r=-1)
+
+        if ax is None:
+            ax = plt.gca()
+
+        sliced.plot(x='phi', y='theta', ax=ax)
+        ax.set_aspect('equal')
+
+
 class MASOutput:
     """
     The results from a single run of MAS.
@@ -26,8 +61,8 @@ class MASOutput:
     def __getattr__(self, attr):
         """
         Get an attribute. This allows one to do e.g. ``masoutput.vr`` to get
-        the radial velocity.
+        the radial velocity variable.
         """
         if attr in _mas_vars:
-            return self._data[attr]
+            return Variable(self._data[attr], attr)
         return super().__getattr__(attr)
