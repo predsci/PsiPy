a2da3f5650e3d3b87be76e1c86084f1706464475
David Stansby <dstansby@gmail.com>
2020-11-08 16:11:12 +0000

    Use RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -29,6 +29,7 @@ author = 'David Stansby'
 # ones.
 extensions = ['sphinx_automodapi.automodapi',
               'sphinx_gallery.gen_gallery',
+              "sphinx_rtd_theme",
               'numpydoc']
 
 numpydoc_show_class_members = False
@@ -51,7 +52,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
