61adf978733929f42c3ea8fa9045ca757bc6f2bb
David Stansby <dstansby@gmail.com>
2020-11-08 16:13:28 +0000

    Use math in sphinx

--- examples/plot_visualising_mas.py
+++ examples/plot_visualising_mas.py
@@ -31,7 +31,7 @@ model.rho.plot_phi_cut(75, ax=ax)
 
 ###############################################################################
 # In the above plot it's hard to see detail at large radial distances, due
-# to the ~ 1/r^2 decrease in density with distance. We can rescale the data,
+# to the :math:`1/r^{2}` decrease in density with distance. We can rescale the data,
 # and plot it again.
 scaled_rho = model.rho
 scaled_rho.data = scaled_rho.data * scaled_rho.data.coords['r']**2
@@ -40,7 +40,5 @@ scaled_rho.name = 'rho r$^{2}$'
 fig = plt.figure()
 ax = plt.subplot(111, projection='polar')
 scaled_rho.plot_phi_cut(75, ax=ax)
-
-
 
 plt.show()
