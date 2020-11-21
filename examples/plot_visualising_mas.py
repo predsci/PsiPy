55035cba9e443a51e6d65e21569c34fcb3d22b60
David Stansby <dstansby@gmail.com>
2020-11-21 19:31:24 +0000

    Update example

--- examples/plot_visualising_mas.py
+++ examples/plot_visualising_mas.py
@@ -20,25 +20,12 @@ print(model.variables)
 ###############################################################################
 # Plot a cut of the model at a constant radius.
 fig, ax = plt.subplots()
-model.rho.plot_radial_cut(0, ax=ax)
+model['rho'].plot_radial_cut(0, ax=ax)
 
 ###############################################################################
 # Plot a cut of the model at a constant longitude.
 fig = plt.figure()
 ax = plt.subplot(111, projection='polar')
-model.rho.plot_phi_cut(75, ax=ax)
-
-
-###############################################################################
-# In the above plot it's hard to see detail at large radial distances, due
-# to the :math:`1/r^{2}` decrease in density with distance. We can rescale the data,
-# and plot it again.
-scaled_rho = model.rho
-scaled_rho.data = scaled_rho.data * scaled_rho.data.coords['r']**2
-scaled_rho.name = 'rho r$^{2}$'
-
-fig = plt.figure()
-ax = plt.subplot(111, projection='polar')
-scaled_rho.plot_phi_cut(75, ax=ax)
+model['rho'].plot_phi_cut(75, ax=ax)
 
 plt.show()
