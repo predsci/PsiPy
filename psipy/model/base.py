9f91f54223c2274c9b11ea0880143bd4238a657d
David Stansby <dstansby@gmail.com>
2020-11-27 20:37:52 +0000

    Move Variable to it's own file

--- psipy/model/base.py
+++ psipy/model/base.py
@@ -1,6 +1,11 @@
 import abc
 from pathlib import Path
 
+from .variable import Variable
+
+
+__all__ = ['ModelOutput']
+
 
 class ModelOutput(abc.ABC):
     """
@@ -17,8 +22,36 @@ class ModelOutput(abc.ABC):
         self._data = {}
         self._variables = self.get_variables()
 
+    def __getitem__(self, var):
+        if var not in self.variables:
+            raise RuntimeError(f'{var} not in list of known variables: '
+                               f'{self._variables}')
+        if var not in self.loaded_variables:
+            self._data[var] = self.load_file(var)
+
+        if var in self._units:
+            unit = self._units[var][0]
+            data = self._data[var] * self._units[var][1]
+            return Variable(data, var, unit)
+        else:
+            raise RuntimeError('Do not know what units are for '
+                               f'variable "{var}"')
+
     @abc.abstractmethod
     def get_variables(self):
+        """
+        Returns
+        -------
+        list :
+            A list of all variable names present in the directory.
+        """
+        pass
+
+    @abc.abstractmethod
+    def load_file(self, var):
+        """
+        Load data for variable *var*.
+        """
         pass
 
     @property
