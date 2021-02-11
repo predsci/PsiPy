02b94c89bbdd9a1deddca1a2b176df6ce11c66e9
David Stansby <dstansby@gmail.com>
2021-02-11 18:52:26 +0000

    Add cell centered velocity code

--- psipy/model/mas.py
+++ psipy/model/mas.py
@@ -1,5 +1,8 @@
 import astropy.constants as const
 import astropy.units as u
+import numpy as np
+import scipy.interpolate
+import xarray as xr
 
 from .base import ModelOutput
 from psipy.io import read_mas_file, get_mas_variables
@@ -59,3 +62,51 @@ class MASOutput(ModelOutput):
 
     def __str__(self):
         return f"MAS output in directory {self.path}"
+
+    def cell_centered_v(self, extra_phi_coord=False):
+        """
+        Get the velocity vector at the cell centres.
+
+        Because the locations of the vector component outputs
+
+        Parameters
+        ----------
+        extra_phi_coord: bool
+            If `True`, add an extra phi slice.
+        """
+        if not set(['vr', 'vt', 'vp']) <= set(self.variables):
+            raise RuntimeError('MAS output must have the vr, vt, vp variables loaded')
+
+        # Interpolate new radial coordiantes
+        new_rcoord = self['vr'].r_coords
+        vt = scipy.interpolate.interp1d(self['vt'].r_coords,
+                                        self['vt'].data,
+                                        axis=2)(new_rcoord)
+        vp = scipy.interpolate.interp1d(self['vp'].r_coords,
+                                        self['vp'].data,
+                                        axis=2)(new_rcoord)
+
+        # Interpolate new theta coordinates
+        new_tcoord = self['vt'].theta_coords
+        vr = scipy.interpolate.interp1d(self['vr'].theta_coords,
+                                        self['vr'].data,
+                                        axis=1)(new_tcoord)
+        vp = scipy.interpolate.interp1d(self['vp'].theta_coords,
+                                        vp,
+                                        axis=1)(new_tcoord)
+        # Don't need to interpolate phi coords, but get a copy
+        new_pcoord = self['vr'].phi_coords
+
+        if extra_phi_coord:
+            dphi = np.mean(np.diff(new_pcoord))
+            assert np.allclose(new_pcoord[0] + 2 * np.pi, new_pcoord[-1] + dphi)
+
+            new_pcoord = np.append(new_pcoord, new_pcoord[-1] + dphi)
+            vp = np.append(vp, vp[0:1, :, :], axis=0)
+            vt = np.append(vt, vt[0:1, :, :], axis=0)
+            vr = np.append(vr, vr[0:1, :, :], axis=0)
+
+        return xr.DataArray(np.stack([vp, vt, vr], axis=-1),
+                            dims=['phi', 'theta', 'r', 'component'],
+                            coords=[new_pcoord, new_tcoord, new_rcoord,
+                                    ['vp', 'vt', 'vr']])
