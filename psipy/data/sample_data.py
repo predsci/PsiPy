8b7d6b7b576669282d3f777f4d4450aeba423ef6
David Stansby <dstansby@gmail.com>
2022-03-20 12:49:17 +0000

    Use pooch to handle sample data

--- psipy/data/sample_data.py
+++ psipy/data/sample_data.py
@@ -3,21 +3,34 @@ Helper functions for downloading sample model output data.
 """
 from pathlib import Path
 
-from parfive import Downloader
+import pooch
 
-download_dir = Path(__file__).parent / '..' / '..' / 'data'
-data_url = 'https://www.predsci.com/data/runs'
+file_url = 'cr{cr}-medium/hmi_masp_mas_std_0201/{type}/{var}002.hdf'
+cache_dir = pooch.os_cache("psipy")
 
 
-def _get_mas_dl_dir(type):
-    """
-    Parameters
-    ----------
-    type : {'helio', 'corona'}
-    """
-    mas_dir = download_dir / f'mas_{type}'
-    mas_dir.mkdir(parents=True, exist_ok=True)
-    return mas_dir.resolve()
+def _get_url(*, type: str, var: str, cr: int = 2210):
+    return file_url.format(cr=cr, type=type, var=var)
+
+
+registry = {}
+
+# Add consecutive Carrington rotation sample data
+for cr in [2210, 2211]:
+    registry[_get_url(cr=cr, type='helio', var='vr')] = None
+
+
+# Add various variables for helio and corona solutions
+vars = ['rho', 'vr', 'br', 'bt', 'bp']
+for type in ['helio', 'corona']:
+    for var in vars:
+        registry[_get_url(cr=2210, type=type, var=var)] = None
+
+mas_pooch = pooch.create(
+    path=cache_dir,
+    base_url="https://www.predsci.com/data/runs/",
+    registry=registry,
+)
 
 
 def mas_sample_data(type='helio'):
@@ -34,31 +47,10 @@ def mas_sample_data(type='helio'):
     pathlib.Path
         Download directory.
     """
-    if type not in ['helio', 'corona']:
-        raise ValueError('type must be one of ["helio", "corona"]')
-    mas_dl_dir = _get_mas_dl_dir(type)
-    base_url = (f'{data_url}/cr2210-medium/hmi_masp_mas_std_0201/{type}/'
-                '{var}002.hdf')
-
-    # Create a downloader to queue the files to be downloaded
-    dl = Downloader()
-    for var in ['rho', 'vr', 'br', 'bt', 'bp']:
-        file = mas_dl_dir / f'{var}002.hdf'
-        if file.exists():
-            continue
-        else:
-            remote_file = base_url.format(var=var)
-            dl.enqueue_file(remote_file, path=mas_dl_dir)
-
-    # Download the files
-    if dl.queued_downloads > 0:
-        result = dl.download()
-        if len(result.errors):
-            raise RuntimeError(
-                'Failed to download files with the following errors:'
-                f'{result.errors}')
-        dl.download()
-    return mas_dl_dir
+    for var in vars:
+        path = mas_pooch.fetch(_get_url(cr=2210, type=type, var=var),
+                               progressbar=True)
+    return Path(path).parent
 
 
 def mas_helio_timesteps():
@@ -76,22 +68,8 @@ def mas_helio_timesteps():
     pathlib.Path
         Download directory.
     """
-    mas_helio_dir = _get_mas_dl_dir('helio')
-    base_url = (data_url +
-                '/cr{cr}-medium/hmi_masp_mas_std_0201/helio/vr002.hdf')
+    for cr in [2210, 2211]:
+        path = mas_pooch.fetch(_get_url(cr=cr, type='helio', var='vr'),
+                               progressbar=True)
 
-    # Create a downloader to queue the files to be downloaded
-    dl = Downloader()
-    for i, cr in enumerate([2210, 2211]):
-        fname = f'vr00{i+1}.hdf'
-        file = mas_helio_dir / fname
-        if file.exists():
-            continue
-        else:
-            remote_file = base_url.format(cr=cr)
-            dl.enqueue_file(remote_file, path=mas_helio_dir, filename=fname)
-
-    # Download the files
-    if dl.queued_downloads > 0:
-        dl.download()
-    return mas_helio_dir
+    return Path(path).parent
