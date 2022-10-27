3424d10d023adbf4f52673d4afb687642961b92c
David Stansby <dstansby@gmail.com>
2022-10-27 14:48:48 +0100

    Add check on tracing phi coords

--- psipy/tracing/tracing.py
+++ psipy/tracing/tracing.py
@@ -121,7 +121,6 @@ class FortranTracer:
 
         # Normalize step size to radial cell size
         rcoords = grid.zcoords
-        step_size = self.step_size * np.min(np.diff(rcoords))
-        self.tracer = StreamTracer(max_steps, step_size)
+        self.tracer = StreamTracer(max_steps, self.step_size)
         self.tracer.trace(seeds, grid)
         return FieldLines(self.tracer.xs, runit)
