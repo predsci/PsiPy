99b7a199b004fefda139f1e15d53eb7c912ee863
David Stansby <dstansby@gmail.com>
2022-12-04 16:04:18 +0000

    Add function to get pot3d variable names

--- psipy/io/pot3d.py
+++ psipy/io/pot3d.py
@@ -1,12 +1,15 @@
 """
 Tools for reading POT3D output files.
 """
+import glob
 from pathlib import Path
 
 import numpy as np
 import xarray as xr
 
 from .util import read_hdf4
+
+__all__ = ["get_pot3d_variables"]
 
 
 def _read_pot3d(file_path: Path) -> xr.Dataset:
@@ -31,3 +34,28 @@ def _read_pot3d(file_path: Path) -> xr.Dataset:
     # Add time
     data = xr.Dataset({var: xr.DataArray(data=data, coords=coords, dims=dims)})
     return data
+
+
+def get_pot3d_variables(directory):
+    """
+    Return a list of variables present in a given directory.
+
+    Parameters
+    ----------
+    directory :
+        Path to the folder containing the POT3D data files.
+
+    Returns
+    -------
+    var_names : list
+        List of variable names present in the given directory.
+    """
+    files = glob.glob(str(Path(directory) / "*.hdf"))
+    # Get the variable name from the filename
+    var_names = [Path(f).stem for f in files]
+    # Only return unique names
+    var_names = list(set(var_names))
+    if not len(var_names):
+        raise FileNotFoundError(f"No variable files found in {directory}")
+    var_names.sort()
+    return var_names
--- psipy/io/tests/test_pot3d.py
+++ psipy/io/tests/test_pot3d.py
@@ -1,8 +1,12 @@
 import xarray as xr
 
-from psipy.io.pot3d import _read_pot3d
+from psipy.io.pot3d import _read_pot3d, get_pot3d_variables
 
 
 def test_read_pot3d(pot3d_directory):
     ds = _read_pot3d(pot3d_directory / "br.hdf")
     assert isinstance(ds, xr.Dataset)
+
+
+def test_get_var_names(pot3d_directory):
+    assert get_pot3d_variables(pot3d_directory) == ["bp", "br", "bt"]
